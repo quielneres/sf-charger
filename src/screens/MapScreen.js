1c4a1f21dbd1c1299c22fefcf10c9aa4404cc147
quielneres <quielneres@gmail.com>
2025-01-21 22:03:13 -0300

    Load home

--- src/screens/MapScreen.js
+++ src/screens/MapScreen.js
@@ -1,87 +1,121 @@
-import React, { useState } from 'react';
-import { View, StyleSheet, Image } from 'react-native';
-import MapView, { Marker } from 'react-native-maps';
-import { useNavigation } from "@react-navigation/native";
+import React, {useEffect, useState} from 'react';
+import {View, StyleSheet, Image, Text} from 'react-native';
+import MapView, {Marker} from 'react-native-maps';
+import {useNavigation} from '@react-navigation/native';
+import {Card, Modal, Spinner} from '@ui-kitten/components';
 
 const MapScreen = () => {
-  const [chargers, setChargers] = useState([
-    {
-      id: 2,
-      name: 'Eletroposto Sol Fort',
-      latitude: -15.6008448,
-      longitude: -47.6839936,
-      description: 'Eletroposto Sol Fort Plug Charger',
-      opened: '9:00 - 23:00',
-      phone: '61 9 91234567',
-      address: 'Rua 204 Numero 30 Vila Brasilia',
-      amenities: [
-        // {
-        //   name: 'Café',
-        //   icon: 'coffee-outline',
-        // },
-        {
-          name: 'Wifi',
-          icon: 'wifi-outline',
-        },
-        // {
-        //   name: 'Comida',
-        //   icon: 'restaurant-outline',
-        // },
-        // {
-        //   name: 'Conveniencia',
-        //   icon: 'shopping-bag-outline',
-        // },
-        // {
-        //   name: 'Bar',
-        //   icon: 'beer-outline',
-        // },
-      ],
-      connectors: [
-        {
-          id: 1,
-          name: '3 pin',
-        },
+  const [regiaoInicial, setRegiaoInicial] = useState(null);
+  const [markerCoordinate, setMarkerCoordinate] = useState([]);
+  const navigation = useNavigation();
+  const [visible, setVisible] = React.useState(false);
+
+
+
+  useEffect(() => {
+    const carregaDadosDoMapa = async () => {
+      setVisible(true);
+
+      await new Promise(resolve => setTimeout(resolve, 500));
+      setRegiaoInicial({
+        latitude: -15.60084489,
+        longitude: -47.6839936,
+        latitudeDelta: 0.05,
+        longitudeDelta: 0.05,
+      });
+      setMarkerCoordinate([
         {
           id: 2,
-          name: 'CCS2',
-        },
-        {
-          id: 3,
-          name: 'Type 2',
+          name: 'Eletroposto Sol Fort',
+          latitude: -15.6008448,
+          longitude: -47.6839936,
+          description: 'Eletroposto Sol Fort Plug Charger',
+          opened: '9:00 - 23:00',
+          phone: '61 9 91234567',
+          address: 'Rua 204 Numero 30 Vila Brasilia',
+          amenities: [
+            // {
+            //   name: 'Café',
+            //   icon: 'coffee-outline',
+            // },
+            {
+              name: 'Wifi',
+              icon: 'wifi-outline',
+            },
+            // {
+            //   name: 'Comida',
+            //   icon: 'restaurant-outline',
+            // },
+            // {
+            //   name: 'Conveniencia',
+            //   icon: 'shopping-bag-outline',
+            // },
+            // {
+            //   name: 'Bar',
+            //   icon: 'beer-outline',
+            // },
+          ],
+          connectors: [
+            {
+              id: 1,
+              name: '3 pin',
+            },
+            {
+              id: 2,
+              name: 'CCS2',
+            },
+            {
+              id: 3,
+              name: 'Type 2',
+            }
+          ],
         }
-      ],
-    },
-  ]);
-  const navigation = useNavigation();
+      ]);
+    };
+
+    carregaDadosDoMapa();
+  }, []);
+
+  if (!regiaoInicial || !markerCoordinate) {
+    return (
+      <View style={styles.loading}>
+        {/*<Text>Carregando mapa...</Text>*/}
+        <Modal
+          visible={visible}
+          backdropStyle={styles.backdrop}
+          onBackdropPress={() => setVisible(false)}>
+          <Card disabled={true}>
+            <Spinner />
+          </Card>
+        </Modal>
+      </View>
+    );
+  }
+
+  const chargerInfo = {
+    id: 2,
+    name: 'Eletroposto Sol Fort',
+    latitude: -15.6008448,
+    longitude: -47.6839936,
+    description: 'Eletroposto Sol Fort Plug Charger',
+  };
 
   const handleMarkerPress = (chargerInfo) => {
     navigation.navigate('CHARGER', { chargerInfo });
   };
 
+
   return (
     <View style={styles.container}>
-      <MapView
-        style={styles.map}
-        initialRegion={{
-          latitude: -15.6008448,
-          longitude: -47.6839936,
-          latitudeDelta: 0.05,
-          longitudeDelta: 0.05,
-        }}
+      <MapView style={styles.map}
+               initialRegion={regiaoInicial}
       >
-        {chargers.map((charger) => (
+        {markerCoordinate.map((charger) => (
           <Marker
-            key={charger.id}
-            coordinate={{ latitude: charger.latitude, longitude: charger.longitude }}
-            title={charger.name}
-            description={charger.description}
-            onPress={() => handleMarkerPress(charger)}
-          >
-            <Image
-              source={require('../assets/charging-station_12351767.png')}
-              style={styles.markerImage}
-            />
-          </Marker>
+          coordinate={{ latitude: charger.latitude, longitude: charger.longitude }}
+          onPress={() => handleMarkerPress(charger)}
+
+          />
         ))}
       </MapView>
     </View>
@@ -99,6 +133,11 @@ const styles = StyleSheet.create({
   markerImage: {
     width: 30,
     height: 30,
+  },
+  loading: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
 
